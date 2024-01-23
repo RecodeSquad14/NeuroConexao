b3357b4868a015a6e63dbc5fd3927074289cfa1e
Anderson1605R <114451515+Anderson1605R@users.noreply.github.com>
2024-01-23 11:19:35 -0300

    criação de rotas administrativas e adição da prop onclick para o botão

--- src/components/button/Button.jsx
+++ src/components/button/Button.jsx
@@ -1,9 +1,9 @@
 import Link from "next/link";
 import React from "react";
 
-function Button({ href, name, className }) {
+function Button({ href, name, className, onClick }) {
   return (
-    <div className={className}>
+    <div className={className} onClick={onClick}>
       <Link href={href}>{name}</Link>
     </div>
   );
