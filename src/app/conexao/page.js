726aaf4f9165c51ba5da0aabbfb54c0ebfa7b73d
Ana Clara <anaamorimsantos1@gmail.com>
2024-01-23 17:02:31 -0300

    alterações na página de conexão, adição de imagem e buttom

--- src/app/conexao/page.js
+++ src/app/conexao/page.js
@@ -1,14 +1,31 @@
 import Card from "@/components/card/card";
 import React from "react";
+import Image from "next/image";
+import Button from "@/components/button/Button";
 
-
-
-function page() {
+function Page() {
   return (
-    <div className="bg-primary h-screen">
-      <Card/>
+    <div className="bg-primary min-h-screen flex flex-col items-center justify-center">
+      <div className="text-center">
+        <Image
+          src="/conexao-img/profissionalNeurotipico.png"
+          alt="grupo de pessoas"
+          width={300}
+          height={300}
+          className="mx-auto max-w-full h-auto p-1 mb-4"
+        />
+        
+        <Card />
+      </div>
+      <div className="mt-6">
+        <Button
+          name="Cadastre-se"
+          href="#"
+          className="bg-transparent w-32 border-white border-solid border-2 text-white rounded-xl p-1 text-center font-bold hover:bg-secondary"
+        />
+      </div>
     </div>
   );
 }
 
-export default page;
+export default Page;
--- src/components/card/card.jsx
+++ src/components/card/card.jsx
@@ -4,10 +4,15 @@ import GridContainer from '../grid/GridContainer'
 function Card() {
   return (
     <div>
-        <GridContainer className="bg-secondary flex flex-col gap-7 justify-center items-center h-40 md:flex-row">
-        <p>texto texto mutcho texto</p>
-        </GridContainer>
+  <GridContainer className="bg-secondary flex flex-col gap-7 justify-center items-center h-40 md:flex-row rounded-md text-white text-lg">
+   
+    <p>Seja a mudança que você quer ver no mundo profissional. Contamos com você para construir um futuro onde a diversidade é o nosso maior trunfo! Seja profissional neurodiverso, Empresa ou Profissional da saúde, cadrastre-se abaixo!</p>
+    <div className="flex items-center justify-center">
+      
     </div>
+  </GridContainer>
+</div>
+
   )
 }
 
