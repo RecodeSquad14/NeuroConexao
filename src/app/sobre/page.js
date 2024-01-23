026694a573fe4de33ccbb084478a70fe0f8ad715
Alfannya <alfannya.2014@gmail.com>
2024-01-23 00:44:35 -0300

    modificações pag. sobre

--- src/app/sobre/page.js
+++ src/app/sobre/page.js
@@ -5,9 +5,34 @@ import React from "react";
 const Sobre = () => {
   return (
     <div className="bg-primary">
-      <h1>Sobre Nós - NeuroConexão</h1>
-
+      <GridContainer className="flex flex-col items-center h-screen">
+        <Image
+          src="/home-img/equipeneura.png"
+          alt="imagem de equipe"
+          width={400}
+          height={160}
+          className="w-full h-auto object-cover"
+        />
+       
+  <p className="mt-5 text-4x1 text-white text-center max-w-2x1">
+        Nosso time é liderado por uma equipe dedicada e diversificada,
+          participante do bootcamp 2023 da RECODE PRO, representando o Squad 14.
+          Nossos antecedentes variados e a experiência adquirida moldam uma
+          equipe que acredita na capacidade de transformar o
+          mundo corporativo por meio da inclusão.
+        </p>
+        
+      </GridContainer>
+      <div className="bg-secondary">
       <GridContainer className="flex gap-3 h-screen">
+        <p className="grid place-items-center text-4xl text-left text-white">
+        A NeuroConexão tem como missão principal resolver o desafio da
+          empregabilidade para pessoas neurodiversas, criando um ambiente
+          inclusivo tanto para empresas quanto para profissionais. Acreditamos
+          que a diversidade é a força propulsora de um futuro mais promissor, e,
+          com a ajuda de profissionais da saúde, estamos caminhando para um
+          mercado de trabalho mais inclusivo.
+        </p>
         <Image
           src="/heroImg.png"
           alt="imagem"
@@ -15,60 +40,22 @@ const Sobre = () => {
           height={300}
           className="w-1/2"
         />
-        <p className="grid place-items-center w1/2 text-4xl text-left text-white">
-          A NeuroConexão surgiu da aspiração de criar oportunidades e promover a
-          inclusão no mercado de trabalho para pessoas neurodiversas.
-          Identificamos uma lacuna significativa nesse cenário,
-          impulsionando-nos a ser uma plataforma dedicada e comprometida com a
-          diversidade.
-        </p>
-      </GridContainer>
+        </GridContainer>
+        </div>
 
-      <section>
-        <h2>Formação:</h2>
-        <p>
-          Nosso time é liderado por uma equipe dedicada e diversificada,
-          participante do bootcamp 2023 da RECODE PRO, representando o Squad 14.
-          Nossos antecedentes variados e a experiência adquirida moldam uma
-          liderança que acredita profundamente na capacidade de transformar o
-          mundo corporativo por meio da inclusão.
-        </p>
-      </section>
+        <GridContainer className="flex gap-3 h-screen">
+        <Image
+          src="/home-img/team.png"
+          alt="imagem"
+          width={300}
+          height={300}
+          className="w-full"
+        />
 
-      <section>
-        <h2>Evolução:</h2>
-        <p>
-          Desde o início, enfrentamos desafios como comunicação e divergências
-          de ideias. Cada obstáculo se tornou uma oportunidade para crescer e
-          aprimorar nosso projeto. Estamos constantemente evoluindo, aprendendo
-          com cada desafio, e hoje, orgulhamo-nos de oferecer uma plataforma
-          cheia de personalidade e cumprindo seu papel.
-        </p>
-      </section>
-
-      <section>
-        <h2>Missão e Visão:</h2>
-        <p>
-          A NeuroConexão tem como missão principal resolver o desafio da
-          empregabilidade para pessoas neurodiversas, criando um ambiente
-          inclusivo tanto para empresas quanto para profissionais. Acreditamos
-          que a diversidade é a força propulsora de um futuro mais promissor, e,
-          com a ajuda de profissionais da saúde, estamos caminhando para um
-          mercado de trabalho mais inclusivo.
-        </p>
-      </section>
-
-      <section>
-        <h2>Compromisso com a Diversidade:</h2>
-        <p>
-          Comprometemo-nos não apenas a fornecer soluções inovadoras, mas também
-          a criar um ambiente que celebre as diferenças. Ao escolher a
-          NeuroConexão, você não apenas investe em uma plataforma de
-          empregabilidade, mas se junta a uma comunidade determinada a construir
-          um futuro onde todos tenham oportunidades.
-        </p>
-      </section>
+      
+        </GridContainer>
     </div>
+    
   );
 };
 
