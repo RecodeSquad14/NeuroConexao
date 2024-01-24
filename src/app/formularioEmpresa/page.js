77916a1624f2d345092adafc488e1a85bd3d71b6
Marcos Custodio <mpdcustodio@gmail.com>
2024-01-24 15:21:40 -0300

    Criação do formulário Empresa

---
+++ src/app/formularioEmpresa/page.js
@@ -0,0 +1,14 @@
+import CadastroEmpresa from '@/components/formulario/CadastroEmpresa';
+
+
+
+const FormularioEmpresa = () => {
+  return (
+    <div>
+      <CadastroEmpresa /> 
+    </div>
+    
+  );
+};
+
+export default FormularioEmpresa;
